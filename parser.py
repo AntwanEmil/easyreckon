29f8cc76ada8ed981175fdd729e0444cfcde5dd8
AntwanEmil <antwanemile@gmail.com>
2022-08-13 23:16:14 +0200

    supressing pylint code rate

--- parser.py
+++ parser.py
@@ -11,40 +11,40 @@ import re
 
 
 def printLinks(url):
- print("===============================================")
- ab = anonBrowser()
- ab.anonymize()
- page = ab.open(url)
- html = page.read()
- try:
-  print('[+]================= Printing Links From Regex.=====================')
-  link_finder = re.compile('href="(.*?)"')
-  links = link_finder.findall(html)
-  for link in links:
-   print(link)
- except:
-  pass
- try:
-  print('\n[+]================ Printing Links From BeautifulSoup.================')
-  soup = BeautifulSoup(html)
-  links = soup.findAll(name='a')
-  for link in links:
-   if link.has_key('href'):
-    print(link['href'])
- except:
-  pass
+  print("===============================================")
+  ab = anonBrowser()
+  ab.anonymize()
+  page = ab.open(url)
+  html = page.read()
+  try:
+    print('[+]================= Printing Links From Regex.=====================')
+    link_finder = re.compile('href="(.*?)"')
+    links = link_finder.findall(html)
+    for link in links:
+      print(link)
+  except:
+    pass
+  try:
+    print('\n[+]================ Printing Links From BeautifulSoup.================')
+    soup = BeautifulSoup(html)
+    links = soup.findAll(name='a')
+    for link in links:
+      if link.has_key('href'):
+        print(link['href'])
+  except:
+    pass
 
 def main():
- parser = optparse.OptionParser('usage%prog ' +\
- '-u <target url>')
- parser.add_option('-u', dest='tgtURL', type='string',\
- help='specify target url')
- (options, args) = parser.parse_args()
- url = options.tgtURL
- if url == None:
-  print(parser.usage)
-  exit(0)
- else:
-  printLinks(url)
+  parser = optparse.OptionParser('usage%prog ' +\
+  '-u <target url>')
+  parser.add_option('-u', dest='tgtURL', type='string',\
+  help='specify target url')
+  (options, args) = parser.parse_args()
+  url = options.tgtURL
+  if url == None:
+    print(parser.usage)
+    exit(0)
+  else:
+    printLinks(url)
 if __name__ == '__main__':
- main()
+  main()
